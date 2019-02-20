7884814d15e405e3697f481ebf5573f34acf8915
unknown <mgarciareimers@gmail.com>
2019-02-20 18:40:09 +0100

    Start of project

---
+++ FirstRound/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	private static String inputFileName = "", // Root of the input file.
+				   outputFileName = ""; // Root of the output file.
+	
+	public static void main(String[] args) {
+		// Read the input file.
+		// TODO - Send all required fields as input.
+		Utilities.readFile(inputFileName);
+		
+		// TODO.
+		Utilities.writeFile(outputFileName);
+	}
+}
